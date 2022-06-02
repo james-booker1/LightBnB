1abbe68774b297ae85b8e5557faeb42fddcd1ef0
James Booker <98477043+james-booker1@users.noreply.github.com>
2022-06-01 19:35:52 -0700

    adds  queries

---
+++ 1_queries/1_details_of_single_user.sql
@@ -0,0 +1,10 @@
+SELECT
+  id,
+  name,
+  email,
+  PASSWORD
+
+FROM
+  users
+WHERE
+  email = 'tristanjacobs@gmail.com'
---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,18 @@
+SELECT
+  properties.id,
+  title,
+  cost_per_night,
+  avg(property_reviews.rating) AS average_rating
+FROM
+  properties
+  JOIN property_reviews ON properties.id = property_id
+WHERE
+  city LIKE '%ancouver%'
+GROUP BY
+  properties.id
+HAVING
+  avg(property_reviews.rating) >= 4
+ORDER BY
+  cost_per_night ASC
+LIMIT
+  10;
